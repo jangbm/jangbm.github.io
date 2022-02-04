3ae676fe3e616733781db37172bb27fc41429e70
jangbm <jang2620@naver.com>
2022-02-04 23:24:27 +0900

    init 2.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-//alert("hello");
+const title = document.querySelector(".class:first-child");
+
+const handleTitleClick = () => {
+    console.log("click");
+    title.style.color = "#ff22ff";
+    title.style.fontSize = "50px";
+};
+
+title.addEventListener("click", handleTitleClick);
+
+const handleResize = () => {
+    console.log(window.innerWidth);
+};
+
+window.addEventListener("resize", handleResize);
+console.dir(window);
