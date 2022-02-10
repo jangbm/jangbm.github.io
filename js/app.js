fc4a514fe2c5aa89f94e0e275e9e284a2a08cf94
jangbm <jang2620@naver.com>
2022-02-10 23:04:42 +0900

    2.0 Storage

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
-const title = document.querySelector(".class:first-child");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const handleTitleClick = () => {
-    console.log("click");
-    title.style.color = "#ff22ff";
-    title.style.fontSize = "50px";
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+const onLoginSubmit = (event) => {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGretting(userName);
 };
 
-title.addEventListener("click", handleTitleClick);
-
-const handleResize = () => {
-    console.log(window.innerWidth);
+const paintGretting = (username) => {
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
-window.addEventListener("resize", handleResize);
-console.dir(window);
+const saveUserName = localStorage.getItem(USERNAME_KEY);
+
+if (saveUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGretting(saveUserName);
+}
