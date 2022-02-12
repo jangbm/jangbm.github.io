a2dde8546353672f6a8db4f61c1ba9aa828b78f4
jangbm <jang2620@naver.com>
2022-02-12 23:36:32 +0900

    # remove

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["01.jpg", "02.jpg", "03.jpg"];
-
-const choseImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `images/${choseImage}`;
-
-document.body.appendChild(bgImage);
